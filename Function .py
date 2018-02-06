d5a0eb1ef34440b8c6673f564ec6abb689f8158b
kareem171 <34690989+kareem171@users.noreply.github.com>
2018-02-06 14:19:41 -0500

    Add files via upload

---
+++ Function .py
@@ -0,0 +1,21 @@
+'''
+passage_characters= "This is a paragraph from the book"
+print(len(passage_characters))
+'''
+#calculate the area of the triangle
+'''
+def triangle_area(base, height):
+    area=(1.0/2)*base*heght
+    return area
+
+print(triangle_area(7,4))
+'''
+
+#square
+def square(value):
+    new_value=value**2
+    print(new_value)
+
+
+
+square(float(input("Enter a value to be squared: ")))
